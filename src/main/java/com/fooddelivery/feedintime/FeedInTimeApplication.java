5c90306ca55caa2a9c5807033abcf93177e0d187
Idan <idanah24@gmail.com>
2020-11-08 18:46:39 +0200

    Setup

---
+++ src/main/java/com/fooddelivery/feedintime/FeedInTimeApplication.java
@@ -0,0 +1,13 @@
+package com.fooddelivery.feedintime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedInTimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedInTimeApplication.class, args);
+	}
+
+}
